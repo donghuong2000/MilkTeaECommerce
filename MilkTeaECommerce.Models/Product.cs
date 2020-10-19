e4a98c1728df36ba02f6c7cda49c38e9fb7196fa
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-10-19 14:30:26 +0700

    Add project files.

---
+++ MilkTeaECommerce.Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilkTeaECommerce.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public long price { get; set; }
+        public string description { get; set; }
+    }
+}
