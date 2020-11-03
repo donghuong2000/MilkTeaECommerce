45eba7bb3640588ca6779880411f5240534ef8af
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-11-03 15:27:23 +0700

    add Repository Parrtern

---
+++ MilkTeaECommerce.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using MilkTeaECommerce.Data;
+using MilkTeaECommerce.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilkTeaECommerce.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+           
+        }
+
+
+       
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
