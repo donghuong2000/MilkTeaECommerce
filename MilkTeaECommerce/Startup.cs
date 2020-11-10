646bfe16b8ac38c3a126a705a01f3dbb6aaddf36
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-11-10 17:58:01 +0700

    update startup

--- MilkTeaECommerce/Startup.cs
+++ MilkTeaECommerce/Startup.cs
@@ -12,6 +12,7 @@ using MilkTeaECommerce.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MilkTeaECommerce.Models;
 
 namespace MilkTeaECommerce
 {
@@ -30,8 +31,12 @@ namespace MilkTeaECommerce
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddUserManager<UserManager<ApplicationUser>>()
+                .AddSignInManager<SignInManager<ApplicationUser>>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+                
             services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
