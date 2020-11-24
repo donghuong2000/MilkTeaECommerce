ae83e96586cb83091feeb66efb2a8d80b907a40f
Trần Thanh Toàn <thanhtontran115@gmail.com>
2020-11-24 13:19:52 +0700

    ProductController CRUD

---
+++ MilkTeaECommerce/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MilkTeaECommerce.Data;
+using MilkTeaECommerce.Models;
+
+namespace MilkTeaECommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Products
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var list = _context.Products.ToList();
+            return Json(new { data = list });
+        }
+        public IActionResult GetforSelect(string q)
+        {
+            var obj = _context.Products.ToList()
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    ImageUrl = x.ImageUrl,
+                    Price = x.Price,
+                    Status = x.Status,
+                    Quantity = x.Quantity,
+                    CategoryId = x.CategoryId,
+                    ShopID = x.ShopId,
+
+                });
+            if (!(string.IsNullOrEmpty(q) || string.IsNullOrWhiteSpace(q)))
+            {
+                obj = obj.Where(x => x.Name.ToLower().StartsWith(q.ToLower())).ToList();
+            }
+            return Json(new { items = obj });
+        }
+        // GET: Admin/Products/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Shop)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Products/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            ViewData["ShopId"] = new SelectList(_context.Shops, "ApplicationUserId", "ApplicationUserId");
+            return View();
+        }
+
+        // POST: Admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ImageUrl,Price,Status,Quantity,CategoryId,ShopId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            ViewData["ShopId"] = new SelectList(_context.Shops, "ApplicationUserId", "ApplicationUserId", product.ShopId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            ViewData["ShopId"] = new SelectList(_context.Shops, "ApplicationUserId", "ApplicationUserId", product.ShopId);
+            return View(product);
+        }
+
+        // POST: Admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Description,ImageUrl,Price,Status,Quantity,CategoryId,ShopId")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            ViewData["ShopId"] = new SelectList(_context.Shops, "ApplicationUserId", "ApplicationUserId", product.ShopId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Shop)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(string id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MilkTeaECommerce/wwwroot/Admin/js/Products.js
@@ -0,0 +1,192 @@
+﻿$(document).ready(function () {
+    $('#productTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Products/GetAll"
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "decription" },
+            { "data": "image" },
+            { "data": "status" },
+            { "data": "quantity" },
+            { "data": "category" },
+            { "data": "shopid" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                             <div class="text-center">
+                                <a href="#" data-toggle="modal" data-target="#EditModalProduct" data-whatever="${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a onClick=Delete("/Admin/Products/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </div>                           
+                            
+                           `;
+                }, "width": "40%"
+            }
+        ]
+
+    });
+
+    $('.selectlist').select2({
+        placeholder: "Select a product",
+        minimumInputLength: 1,
+        ajax: {
+            url: '/Admin/Products/GetforSelect',
+            data: function (params) {
+                return {
+                    q: params.term// search term
+                };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.items
+                }
+            },
+        }
+
+
+
+    });
+
+
+
+});
+
+$('#CreateSubmitProduct').click(function () {
+    var Id = $('#Create_productid').val();
+    var Name = $('#create_productname').val();
+    var Description = $('#create_productdescription').val();
+    var ImageUrl = $('#create_productimage').val();
+    var Price = $('#create_productprice').val();
+    var Status = $('#create_productstatus').val();
+    var Quantity = $('#create_productquantity').val();
+    var Category = $('#create_productcategory').val();
+    var ShopId = $('#create_productshopid').val();
+    $.ajax({
+        method: 'POST',
+        url: "/Admin/Products/Create",
+        data: { id: Id, name: Name, description: Description, imageUrl: ImageUrl, price: Price, status: Status, quantity: Quantity, category: Category, shopId: ShopId },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                $('#dataTable').DataTable().ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    })
+})
+$('#editSubmitProduct').click(function () {
+    var oldId = $('#update_deliveriesid_old').val();
+    var newId = $('#update_deliveriesid_new').val();
+    var Name = $('#update_deliveriesname').val();
+    var Description = $('#update_productdescription').val();
+    var ImageUrl = $('#update_productimage').val();
+    var Price = $('#update_productprice').val();
+    var Status = $('#update_productstatus').val();
+    var Quantity = $('#update_productquantity').val();
+    var Category = $('#update_productcategory').val();
+    var ShopId = $('#update_productshopid').val();
+    $.ajax({
+        method: 'POST',
+        url: "/Admin/Products/Update",
+        data: { newid: newId, newname: Name, description: Description, imageUrl: ImageUrl, price: Price, status: Status, quantity: Quantity, category: Category, shopId: ShopId },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                $('#dataTable').DataTable().ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    })
+})
+$('#EditModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var ad = button.data('whatever') // Extract info from data-* attributes
+    var modal = $(this)
+    $.ajax({
+        method: "GET",
+        url: '/Admin/Products/Get/' + ad,
+        success: function (data) {
+            console.log(data)
+            modal.find('#update_productid_old').val(data.data.id)
+            modal.find('#update_deliveriesid_new').val(data.data.id)
+            modal.find('#update_productname').val(data.data.name)
+            modal.find('#update_productdescription').val(data.data.description)
+            modal.find('#update_productimage').val(data.data.imageUrl)
+            modal.find('#update_productprice').val(data.data.price)
+            modal.find('#update_productstatus').val(data.data.status)
+            modal.find('#update_productquantity').val(data.data.quantity)
+            modal.find('#update_productcategory').val(data.data.category)
+            modal.find('#update_productshopid').val(data.data.shopId)
+            //modal.find().val(data.data[0].id)
+        }
+    })
+
+
+
+})
+
+
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+function Delete(url) {
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        swalWithBootstrapButtons.fire(
+                            'Deleted!',
+                            data.message,
+                            'success'
+                        );
+                        $('#dataTable').DataTable().ajax.reload();
+                    }
+                    else {
+                        swalWithBootstrapButtons.fire(
+                            'Error',
+                            data.message,
+                            'error'
+                        )
+                    }
+                }
+
+            })
+
+        }
+        else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Your record is safe :)',
+                'error'
+            )
+        }
+    })
+}
