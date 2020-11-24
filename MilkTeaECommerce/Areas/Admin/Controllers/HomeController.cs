1c63002d9f77d7f8d7e3c87ee82201f7e3b6e97a
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-11-24 12:54:07 +0700

    fix template

--- MilkTeaECommerce/Areas/Admin/Controllers/HomeController.cs
+++ MilkTeaECommerce/Areas/Admin/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MilkTeaECommerce.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles = "Admin")]
+   // [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         public IActionResult Index()
