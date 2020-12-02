47eb6c524474c71858e0109574d879c0b2896a49
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-12-02 12:44:58 +0700

    pagenavigation

---
+++ MilkTeaECommerce/Controllers/CheckoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MilkTeaECommerce.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        [HttpGet]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Detail(string a)
+        {
+            return View();
+        }
+    }
+}
---
+++ MilkTeaECommerce/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MilkTeaECommerce.Data;
+
+namespace MilkTeaECommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Detail(string id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("index", "home");
+            }
+            else
+            {
+                var product = _db.Products.Include(x=>x.Category).Include(x=>x.Shop).FirstOrDefault(x => x.Id == id);
+                if(product!=null)
+                {
+                    return View(product);
+                }
+                return NotFound();
+            }
+            
+        }
+    }
+}
