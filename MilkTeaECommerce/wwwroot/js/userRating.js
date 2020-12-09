a97dc59aaee133b32217a3268ea2c8c3382735f5
Phạm Hoàng <hoangcf7a2@gmail.com>
2020-12-09 20:08:56 +0700

    Update nút đánh giá và chỉnh màu cho các nút

--- MilkTeaECommerce/wwwroot/js/userRating.js
+++ MilkTeaECommerce/wwwroot/js/userRating.js
@@ -57,28 +57,60 @@ $('#RatingModal').on('show.bs.modal', function (event) {
             console.log(rate)
             if (rate == "star0")
             {
-                document.getElementById("star1").checked = false;
-                document.getElementById("star2").checked = false;
-                document.getElementById("star3").checked = false;
-                document.getElementById("star4").checked = false; // neu get element by id khong get ra duoc thi nhung cau lenh phia sau se khong thuc hien duoc
-                modal.find("#ratingrate").html(0);
+                document.getElementById('star1').checked = false;
+                document.getElementById('star2').checked = false;
+                document.getElementById('star3').checked = false;
+                document.getElementById('star4').checked = false; // neu get element by id khong get ra duoc thi nhung cau lenh phia sau se khong thuc hien duoc
+                modal.find('#ratingrate').html(0);
             }
             else
             {
                 document.getElementById(rate).checked = true;
-                modal.find("#ratingrate").html(Math.floor(data.data.rating));
-            }
-            
-            if (data.data.status == "evaluated") {
-                modal.find("submit_rating_modal").attr("hidden",true)
             }
             
 
         }
     })
-
-
-
 })
 
+
+$('#RatingModalReadOnly').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var productId = button.data('productid') // Extract info from data-* attributes 
+    var orderdetailId = button.data('orderdetailid')
+    var modal = $(this)
+    $.ajax({
+        method: "GET",
+        url: "/Rating/Get",
+        data: { orderdetailid: orderdetailId, productid: productId },
+        success: function (data) {
+            console.log(data)
+            console.log(orderdetailId)
+            modal.find('#orderdetailidreadonly').val(data.data.orderdetailid)
+            modal.find('#ratingproductidreadonly').val(data.data.productid)
+            modal.find('#ratinguseridreadonly').val(data.data.user)
+            modal.find('#ratingimagereadonly').attr('src', data.data.productimage)
+            modal.find('#ratingnamereadonly').html(data.data.productname)
+            modal.find('#ratingcategoryreadonly').html(data.data.productcategory)
+            modal.find('#ratingcontentreadonly').html(data.data.content)
+            var rate = "star" + Math.floor(data.data.rating).toString()
+            console.log(rate)
+            if (rate == "star0") {
+                modal.find('#ratingratereadonly').html(0);
+                document.getElementById("star1readonly").checked = false;
+                document.getElementById("star2readonly").checked = false;
+                document.getElementById("star3readonly").checked = false;
+                document.getElementById("star4readonly").checked = false; // neu get element by id khong get ra duoc thi nhung cau lenh phia sau se khong thuc hien duoc
+                
+            }
+            else {
+                modal.find('#ratingratereadonly').html(Math.floor(data.data.rating));
+                document.getElementById(rate+"readonly").checked = true;
+               
+            }
+
+
+        }
+    })
+})
 
