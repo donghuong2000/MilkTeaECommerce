711058f5156f2944d5db7c7e0183ed3888a0da54
Thuyet Y <ykimcnc@gmail.com>
2020-12-15 00:46:46 +0700

    product, delivery, category paging

--- MilkTeaECommerce/Controllers/DiscountsController.cs
+++ MilkTeaECommerce/Controllers/DiscountsController.cs
@@ -18,7 +18,141 @@ namespace MilkTeaECommerce.Controllers
         }
         public IActionResult Index(int? page)
         {
+
             return View();
+        }
+        public IActionResult Product(int? page)
+        {
+            ViewBag.Name = "Product";
+            int num = 9;
+            var listDiscount = _db.Discounts.Where(x => x.ProductDiscount.Count > 0);
+
+            int discountCount = listDiscount.Count();
+            // điều kiện để phân trang
+            if (page != null && page > 0)
+            {
+                int NumPageCanHave = discountCount % num == 0 ? discountCount / num : (discountCount / num) + 1;
+
+                ViewBag.page = page;
+                // nếu là trang cuối và là rơi vào trường hơp 2
+                if (page >= NumPageCanHave && discountCount % num != 0)
+                {
+
+                    // lấy số lẻ
+                    listDiscount = listDiscount.Skip(num * (NumPageCanHave - 1)).Take(discountCount % num);
+
+                }
+                //trường hợp 1
+                else
+                {
+
+                    listDiscount = listDiscount.Skip(num * (page.GetValueOrDefault() - 1)).Take(num);
+                }
+            }
+            else
+            {
+                ViewBag.page = 1;
+                listDiscount = listDiscount.Take(9);
+            }
+            // chuyển product thành ProductViewModel
+            var obj = listDiscount
+                .Select(x => new DiscountViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Code = x.Code,
+                    DateStart = x.DateStart
+                }).ToList();
+            return View(obj);
+        }
+        public IActionResult Delivery(int? page)
+        {
+            ViewBag.Name = "Delivery";
+            int num = 9;
+            var listDiscount = _db.Discounts.Include(x => x.DeliveryDiscount)
+                .OrderByDescending(x => x.DateStart).Where(x => x.DeliveryDiscount.Count > 0);
+
+            int discountCount = listDiscount.Count();
+            // điều kiện để phân trang
+            if (page != null && page > 0)
+            {
+                int NumPageCanHave = discountCount % num == 0 ? discountCount / num : (discountCount / num) + 1;
+
+                ViewBag.page = page;
+                // nếu là trang cuối và là rơi vào trường hơp 2
+                if (page >= NumPageCanHave && discountCount % num != 0)
+                {
+
+                    // lấy số lẻ
+                    listDiscount = listDiscount.Skip(num * (NumPageCanHave - 1)).Take(discountCount % num);
+
+                }
+                //trường hợp 1
+                else
+                {
+
+                    listDiscount = listDiscount.Skip(num * (page.GetValueOrDefault() - 1)).Take(num);
+                }
+            }
+            else
+            {
+                ViewBag.page = 1;
+                listDiscount = listDiscount.Take(9);
+            }
+            // chuyển product thành ProductViewModel
+            var obj = listDiscount
+                .Select(x => new DiscountViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Code = x.Code,
+                    DateStart = x.DateStart
+                }).ToList();
+            return View("Product", obj);
+        }
+        public IActionResult Category(int? page)
+        {
+            ViewBag.Name = "Category";
+            int num = 9;
+            var listDiscount = _db.Discounts.Where(x => x.CategoryDiscount.Count > 0);
+
+            int discountCount = listDiscount.Count();
+            // điều kiện để phân trang
+            if (page != null && page > 0)
+            {
+                int NumPageCanHave = discountCount % num == 0 ? discountCount / num : (discountCount / num) + 1;
+
+                ViewBag.page = page;
+                // nếu là trang cuối và là rơi vào trường hơp 2
+                if (page >= NumPageCanHave && discountCount % num != 0)
+                {
+
+                    // lấy số lẻ
+                    listDiscount = listDiscount.Skip(num * (NumPageCanHave - 1)).Take(discountCount % num);
+
+                }
+                //trường hợp 1
+                else
+                {
+
+                    listDiscount = listDiscount.Skip(num * (page.GetValueOrDefault() - 1)).Take(num);
+                }
+            }
+            else
+            {
+                ViewBag.page = 1;
+                listDiscount = listDiscount.Take(9);
+            }
+            // chuyển product thành ProductViewModel
+            var obj = listDiscount
+                .Select(x => new DiscountViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Code = x.Code,
+                    DateStart = x.DateStart
+                }).ToList();
+            return View("Product", obj);
         }
         public IActionResult productdiscount(int? page)
         {
@@ -66,7 +200,8 @@ namespace MilkTeaECommerce.Controllers
         public IActionResult deliverydiscount(int? page)
         {
             int num = 9;
-            var listDiscount = _db.Discounts.OrderByDescending(x=>x.DateStart).Where(x => x.DeliveryDiscount.Count > 0);
+            var listDiscount = _db.Discounts.Include(x=>x.DeliveryDiscount)
+                .OrderByDescending(x=>x.DateStart).Where(x => x.DeliveryDiscount.Count > 0);
 
             int discountCount = listDiscount.Count();
             // điều kiện để phân trang
