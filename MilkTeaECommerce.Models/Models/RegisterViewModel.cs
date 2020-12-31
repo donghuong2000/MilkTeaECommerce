01d330f2747d6b19aa6c991b911abe2528b9fa9e
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-12-31 12:08:34 +0700

    fix giao diện

--- MilkTeaECommerce.Models/Models/RegisterViewModel.cs
+++ MilkTeaECommerce.Models/Models/RegisterViewModel.cs
@@ -9,6 +9,7 @@ namespace MilkTeaECommerce.Models.Models
     public class RegisterViewModel
     {
         [Required(ErrorMessage ="Phải nhập tên cho User")]
+        [Display(Name="Họ và tên")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress,ErrorMessage ="Email không hợp lệ")]
@@ -17,19 +18,22 @@ namespace MilkTeaECommerce.Models.Models
         
         [Required(ErrorMessage = "Phải nhập sdt")]
         [MinLength(8,ErrorMessage ="SDT không hợp lệ")]
+        [Display(Name = "Số điện thoại")]
         public string Sdt { get; set; }
 
         [Required(ErrorMessage = "Phải nhập UserName")]
+        [Display(Name = "Tên tài khoản")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Phải nhập Pass")]
         [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
         public string Password { get; set; }
-
+        [Display(Name = "Nhập lại Mật khẩu")]
         [Compare("Password")]
         [DataType(DataType.Password)]
         public string ComfirmPassword { get; set; }
-
+        [Display(Name = "Quyền")]
         public string Role { get; set; }
 
         public IEnumerable<SelectListItem> ListRole { get; set; }
