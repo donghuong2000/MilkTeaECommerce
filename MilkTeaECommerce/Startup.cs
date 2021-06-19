c445a2f423490fcb06c8753b0a9cc4ac2c6467fb
Phạm Hoàng <hoangcf7a2@gmail.com>
2021-06-19 22:00:52 +0700

    fix bug

--- MilkTeaECommerce/Startup.cs
+++ MilkTeaECommerce/Startup.cs
@@ -83,6 +83,11 @@ namespace MilkTeaECommerce
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                await next();
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
