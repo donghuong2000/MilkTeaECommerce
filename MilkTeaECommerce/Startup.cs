2a2d77d095252c8f6584358e27d11a253dcb8285
huong.nguyen <huong.nguyen@kyanon.digital>
2021-06-20 11:01:17 +0700

    fix bug

--- MilkTeaECommerce/Startup.cs
+++ MilkTeaECommerce/Startup.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-using MilkTeaECommerce.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MilkTeaECommerce.Models;
-using MilkTeaECommerce.DataAccess.Repository.IRepository;
+using MilkTeaECommerce.Data;
 using MilkTeaECommerce.DataAccess.Repository;
-using Microsoft.AspNetCore.Identity.UI.Services;
+using MilkTeaECommerce.DataAccess.Repository.IRepository;
+using MilkTeaECommerce.Models;
 using MilkTeaECommerce.Utility;
+using System;
 
 namespace MilkTeaECommerce
 {
@@ -35,9 +30,14 @@ namespace MilkTeaECommerce
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<ApplicationUser>(options => {
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
                 options.User.RequireUniqueEmail = true;
                 options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
             })
             .AddUserManager<UserManager<ApplicationUser>>()
             .AddSignInManager<SignInManager<ApplicationUser>>()
@@ -45,7 +45,7 @@ namespace MilkTeaECommerce
             .AddEntityFrameworkStores<ApplicationDbContext>();
             services.Configure<EmailOptions>(Configuration.GetSection("EmailOptions"));
             services.AddSingleton<IEmailSender, EmailSender>();
-            
+
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.ConfigureApplicationCookie(options =>
@@ -103,8 +103,8 @@ namespace MilkTeaECommerce
                   areaName: "Shipper",
                   pattern: "Shipper/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapDefaultControllerRoute();
-                
-                
+
+
 
             });
         }
